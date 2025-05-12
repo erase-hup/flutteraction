aa33133bbe3fc0078a8b011541cae75aad693a37
erase-hup <yasin.emrebas@gmail.com>
2025-05-12 15:07:18 +0300

    Delete lib directory

--- lib/core/routing/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static const String homeScreen = "/homeScreen";
-  static const String onBoardingScreen = "/onBoardingScreen";
-  static const String loginScreen = "/LoginScreen";
-  static const String sigUpScreen = "/SigUpScreen";
-}
